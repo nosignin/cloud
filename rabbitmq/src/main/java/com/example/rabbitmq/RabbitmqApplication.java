36146e50cac1dea28c79e46345b2b2d6858e4bac
石佳 <shijia@blockchainrdc.com>
2019-10-31 20:49:12 +0800

    给rabbitmq整合swagger-ui

--- rabbitmq/src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // TODO 完善全局异常捕获内容,全局日志追踪
-// TODO 完善swagger-ui整合
 // TODO 搭建redis项目
 // TODO 搭建zuul项目,丰富ZuulFilter内容,参考:https://blog.csdn.net/weixin_38003389/article/details/83650456
 @SpringBootApplication
---
+++ rabbitmq/src/main/java/com/example/rabbitmq/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.rabbitmq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger-ui地址:http://127.0.0.1:8080/swagger-ui.html
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.rabbitmq.web"))
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("shijia Manage Swagger RESTful APIs")
+                .description("综合管理 Swagger API 服务")
+                .termsOfServiceUrl("http://swagger.io/")
+                .contact(new Contact("shijia", "127.0.0.1", "shijia@163.com.cn"))
+                .version("1.0")
+                .build();
+
+    }
+}
