531e14c46bfa96218ca6f55bac18b5c96dc91af5
石佳 <shijia@blockchainrdc.com>
2019-11-01 16:23:05 +0800

    整合 zuul

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 控制台地址:http://localhost:10000/
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
