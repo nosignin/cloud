13c37d74fea82e0cefbd82fde31f2c00cb1c7bcb
shijia <1623299667@qq.com>
2020-03-06 18:31:44 +0800

    zipkin

---
+++ zipkin-b/src/test/java/com/example/zipkin/b/ZipkinBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipkin.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
