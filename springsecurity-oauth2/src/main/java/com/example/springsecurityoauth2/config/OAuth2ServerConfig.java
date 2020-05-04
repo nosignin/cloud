87d1c8b498745e093a235c88a4d6de220ade957e
shijia <1623299667@qq.com>
2020-05-04 11:42:11 +0800

    Update OAuth2ServerConfig.java

--- springsecurity-oauth2/src/main/java/com/example/springsecurityoauth2/config/OAuth2ServerConfig.java
+++ springsecurity-oauth2/src/main/java/com/example/springsecurityoauth2/config/OAuth2ServerConfig.java
@@ -4,7 +4,6 @@ package com.example.springsecurityoauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
