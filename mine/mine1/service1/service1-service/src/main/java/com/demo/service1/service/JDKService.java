102b52a8076b293a0c911d51f27e830c53c12293
shijia <1623299667@qq.com>
2020-05-04 14:05:24 +0800

    CompletableFuture

---
+++ mine/mine1/service1/service1-service/src/main/java/com/demo/service1/service/JDKService.java
@@ -0,0 +1,13 @@
+package com.demo.service1.service;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author 石佳
+ * @since 2020/05/04/12:21
+ */
+public interface JDKService {
+    void thenAcceptBoth() throws ExecutionException, InterruptedException;
+}
