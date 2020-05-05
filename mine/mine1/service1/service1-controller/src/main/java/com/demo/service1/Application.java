d51f24aa5ac41f19e807f9f23686753dbeff5be0
shijia <1623299667@qq.com>
2020-05-05 18:06:01 +0800

    maven打包问题修复

--- mine/mine1/service1/service1-controller/src/main/java/com/demo/service1/Application.java
+++ mine/mine1/service1/service1-controller/src/main/java/com/demo/service1/Application.java
@@ -1,12 +1,10 @@
 package com.demo.service1;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableDubboConfiguration
 @EnableCaching
 public class Application {
 
