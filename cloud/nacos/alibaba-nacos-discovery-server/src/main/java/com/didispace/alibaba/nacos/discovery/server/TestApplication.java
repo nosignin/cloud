20483f2a01c92fd6f2b2563a25e7294f01e3a564
石佳 <jia.shi@casstime.com>
2020-05-08 13:59:35 +0800

    Update TestApplication.java

--- cloud/nacos/alibaba-nacos-discovery-server/src/main/java/com/didispace/alibaba/nacos/discovery/server/TestApplication.java
+++ cloud/nacos/alibaba-nacos-discovery-server/src/main/java/com/didispace/alibaba/nacos/discovery/server/TestApplication.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
 *  参考：http://blog.didispace.com/spring-cloud-alibaba-1/
- *  先启动nacos,然后进入控制台，http://127.0.0.1:8848/nacos
+ *  下载地址：https://github.com/alibaba/nacos/releases
+ *  版本：1.0.0
+ *  先启动nacos,startup.cmd -m standalone，然后进入控制台，http://127.0.0.1:8848/nacos
  *  启动server的两个实例，后common，然后访问http://localhost:9000/test，会随机轮询
 * @author 石佳
 * @since 2020/5/8
