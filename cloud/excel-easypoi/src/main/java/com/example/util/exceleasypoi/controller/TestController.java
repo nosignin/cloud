6586ed3854982868ed7ff5600fc972ac56153f19
石佳 <jia.shi@casstime.com>
2020-08-06 14:05:59 +0800

    导出excel

--- cloud/excel-easypoi/src/main/java/com/example/util/exceleasypoi/controller/TestController.java
+++ cloud/excel-easypoi/src/main/java/com/example/util/exceleasypoi/controller/TestController.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,11 +24,32 @@ public class TestController {
      * 导入excel
      * 地址在：http://localhost:8080/swagger-ui.html
      */
-    @PostMapping("/import/excel")
+    @PostMapping("/excel/import")
     @ApiOperation(value = "导入excel", notes = "具体描述")
     public List<CustomerList> importExcel(@RequestParam("file") MultipartFile file) {
         List<CustomerList> personList = FileWithExcelUtil.importExcel(file, 1, CustomerList.class);
         log.info(">>> 解析到的用户数据是 {} <<<", JSON.toJSONString(personList));
         return personList;
+    }
+
+
+    /**
+     * 导出excel
+     * 地址在：http://localhost:8080/excel/output
+     */
+    @GetMapping("/excel/output")
+    @ApiOperation(value = "导出excel", notes = "具体描述")
+    public void outputExcel(HttpServletResponse response){
+        try {
+            List<CustomerList> personList = new ArrayList<>();
+            for(int i =0;i<10;i++){
+                CustomerList customerList = new CustomerList();
+                customerList.setName("asdas"+i);
+                personList.add(customerList);
+            }
+            FileWithExcelUtil.exportExcel(personList,CustomerList.class,"客户表.xls",response);
+        } catch (Exception e) {
+            log.info("getCustomerPage", e);
+        }
     }
 }
--- cloud/excel-easypoi/src/main/java/com/example/util/exceleasypoi/util/FileWithExcelUtil.java
+++ cloud/excel-easypoi/src/main/java/com/example/util/exceleasypoi/util/FileWithExcelUtil.java
@@ -1,10 +1,16 @@
 package com.example.util.exceleasypoi.util;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -18,7 +24,6 @@ public class FileWithExcelUtil {
     * @param file 文件
     * @param headerRows 表头行数
     * @param pojoClass 类对象
-    * @return java.util.List<T>
     * @author 石佳
     * @since 2020/8/6
     */
@@ -37,5 +42,37 @@ public class FileWithExcelUtil {
             return null;
         }
         return list;
+    }
+
+    /**
+    *  导出excel
+    * @param list 对象列表
+    * @param pojoClass 对象类型
+    * @param fileName 生成的文件名
+    * @author 石佳
+    * @since 2020/8/6
+    */
+    public static <T> void exportExcel(List<T> list,Class<T> pojoClass, String fileName, HttpServletResponse response) {
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams(),pojoClass,list);
+        if (workbook != null);
+        downLoadExcel(fileName, response, workbook);
+    }
+
+    /**
+    *  下载excel
+    * @param fileName 文件名
+    * @author 石佳
+    * @since 2020/8/6
+    */
+    private static void downLoadExcel(String fileName, HttpServletResponse response, Workbook workbook) {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            response.setHeader("content-Type", "application/vnd.ms-excel");
+            response.setHeader("Content-Disposition",
+                    "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+            workbook.write(response.getOutputStream());
+        } catch (IOException e) {
+            log.error("[monitor][IO][表单功能]", e);
+        }
     }
 }
