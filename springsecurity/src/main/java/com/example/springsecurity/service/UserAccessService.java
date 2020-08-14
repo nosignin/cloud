85204c43e8f403cd42171d6f3df3566607fb9a18
石佳 <jia.shi@casstime.com>
2020-08-14 10:04:55 +0800

    oauth，security删除，技术过时&不够深入

--- springsecurity/src/main/java/com/example/springsecurity/service/UserAccessService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springsecurity.service;
-
-import org.springframework.security.core.Authentication;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface UserAccessService {
-    boolean pass(HttpServletRequest request, Authentication authentication);
-}
